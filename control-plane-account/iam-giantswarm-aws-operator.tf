4b900f091906f0a7acac1a011f6ee8b809019dd5
Simon Weald <glitchcrab-github@simonweald.com>
2020-02-26 14:29:24 +0000

    import generici resource module and amend policy attachment details

--- control-plane-account/iam-giantswarm-aws-operator.tf
+++ control-plane-account/iam-giantswarm-aws-operator.tf
@@ -1,12 +1,17 @@
 # control-plane account-specific resources
 
+# import the generic resources module
+module "giantswarm-prerequisites-generic" {
+  source = "../generic-resources"
+}
+
 resource "aws_iam_user" "giantswarm-aws-operator" {
   name = "GiantSwarmAWSOperator"
 }
 
-resource "aws_iam_role_policy_attachment" "giantswarm-aws-operator-user" {
-  role       = aws_iam_user.giantswarm-aws-operator.name
-  policy_arn = aws_iam_policy.giantswarm-aws-operator.arn
+resource "aws_iam_user_policy_attachment" "giantswarm-aws-operator-user" {
+  user       = aws_iam_user.giantswarm-aws-operator.name
+  policy_arn = module.giantswarm-prerequisites-generic.iam-policy-arn
 }
 
 resource "aws_iam_access_key" "giantswarm-aws-operator-user" {
