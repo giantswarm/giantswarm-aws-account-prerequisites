0910eb7e89234c090afa95b083390cd19b5bc3c3
Simon Weald <glitchcrab-github@simonweald.com>
2020-02-26 15:44:15 +0000

    add tenant cluster account resources

---
+++ tenant-cluster-account/iam-giantswarm-aws-operator-role.tf
@@ -0,0 +1,29 @@
+# tenant cluster account-specific resources
+
+# import the generic resources module
+module "generic-resources" {
+  source = "../generic-resources"
+}
+
+resource "aws_iam_role" "giantswarm-aws-operator" {
+  name = "GiantSwarmAWSOperator"
+  assume_role_policy = data.aws_iam_policy_document.giantswarm-aws-operator.json
+}
+
+resource "aws_iam_role_policy_attachment" "giantswarm-aws-operator" {
+  role       = aws_iam_role.giantswarm-aws-operator.name
+  policy_arn = module.generic-resources.iam-policy-arn
+}
+
+data "aws_iam_policy_document" "giantswarm-aws-operator" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.main_account_id}:user/WealdyTestUser"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
---
+++ tenant-cluster-account/outputs.tf
@@ -0,0 +1,3 @@
+output "aws-operator-role" {
+  value = aws_iam_role.giantswarm-aws-operator.arn
+}
