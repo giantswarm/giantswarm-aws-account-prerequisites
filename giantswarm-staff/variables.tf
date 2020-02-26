cc1269aa9449c8684091a7e05d8cd0ded4428768
Simon Weald <glitchcrab-github@simonweald.com>
2020-02-26 15:51:25 +0000

    move staff role creation to a submodule

--- giantswarm-staff/variables.tf
+++ giantswarm-staff/variables.tf
@@ -1,9 +1,3 @@
-variable "principal_arns_aws_operator" {
-  type        = list(string)
-  description = "ARNs of accounts, groups, or users with the ability to assume this role."
-  default =["arn:aws:iam::084190472784:root"]
-}
-
 variable "principal_arns_giantswarm_staff" {
   type        = list(string)
   description = "ARNs of accounts, groups, or users with the ability to assume this role."
