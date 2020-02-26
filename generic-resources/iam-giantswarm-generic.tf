dbef92c0c64cddce7556204747c7722a1c022e3a
Simon Weald <glitchcrab-github@simonweald.com>
2020-02-26 15:50:55 +0000

    create admin policy and output arn as variable

--- generic-resources/iam-giantswarm-generic.tf
+++ generic-resources/iam-giantswarm-generic.tf
@@ -4,3 +4,8 @@ resource "aws_iam_policy" "giantswarm-aws-operator" {
   name   = "GiantSwarmAWSOperatorPolicy"
   policy = file("${path.module}/iam-policy.json")
 }
+
+resource "aws_iam_policy" "giantswarm-staff" {
+  name   = "GiantSwarmAdminPolicy"
+  policy = file("${path.module}/iam-policy.json")
+}
