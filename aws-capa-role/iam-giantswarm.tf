5589f7281d68ce20eb8d55625d650384d4dfaeb1
Nick Jüttner <nick@giantswarm.io>
2021-04-26 15:25:29 +0200

    include capa

--- aws-capa-role/iam-giantswarm.tf
+++ aws-capa-role/iam-giantswarm.tf
@@ -1,7 +1,5 @@
 locals {
-  controller_role_name   = "GiantSwarmAWSCapaController"
-  node_role_name         = "GiantSwarmAWSCapaNode"
-  controlplane_role_name = "GiantSwarmAWSCapaControlPlane"
+  controller_role_name = "GiantSwarmAWSCapaController"
 }
 
 resource "aws_iam_policy" "giantswarm-aws-capa-controller" {
@@ -29,58 +27,6 @@ data "aws_iam_policy_document" "giantswarm-aws-capa-controller" {
     principals {
       type        = "AWS"
       identifiers = ["${var.arn_prefix}:iam::${var.main_account_id}:user/${var.operator_user_name}"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_policy" "giantswarm-aws-capa-controlplane" {
-  name = local.controlplane_role_name
-  policy = templatefile("${path.module}/iam-controlplane-policy.json", {
-    account_id = "${var.target_account_id}"
-    arn_prefix = "${var.arn_prefix}"
-  })
-}
-
-resource "aws_iam_role" "giantswarm-aws-capa-controlplane" {
-  name               = local.controlplane_role_name
-  assume_role_policy = data.aws_iam_policy_document.giantswarm-aws-capa-controlplane.json
-}
-
-data "aws_iam_policy_document" "giantswarm-aws-capa-controlplane" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_policy" "giantswarm-aws-capa-node" {
-  name = local.node_role_name
-  policy = templatefile("${path.module}/iam-node-policy.json", {
-    account_id = "${var.target_account_id}"
-    arn_prefix = "${var.arn_prefix}"
-  })
-}
-
-resource "aws_iam_role" "giantswarm-aws-capa-node" {
-  name               = local.node_role_name
-  assume_role_policy = data.aws_iam_policy_document.giantswarm-aws-capa-node.json
-}
-
-data "aws_iam_policy_document" "giantswarm-aws-capa-node" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["ec2.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
