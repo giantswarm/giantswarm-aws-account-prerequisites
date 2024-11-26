93c08feb865db3b059e0a04398fb40a1e432dea7
Pau Rosello <pau@giantswarm.io>
2024-11-26 13:20:05 +0100

    add aws partition

--- admin-role/variables.tf
+++ admin-role/variables.tf
@@ -3,6 +3,12 @@ variable "admin_role_name" {
   default = "GiantSwarmAdmin"
 }
 
+variable "aws_partition" {
+  type        = string
+  description = "AWS partition used for ARN referencing, use aws-cn for China regions"
+  default     = "aws"
+}
+
 variable "gs_user_account" {
   type        = string
   description = "AWS account where GS staff users are located"
