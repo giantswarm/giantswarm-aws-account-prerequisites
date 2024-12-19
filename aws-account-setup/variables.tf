5dfcff398b8555d3232c764cd4ee89ac036ab86e
iuri aranda <iuri@giantswarm.io>
2024-12-19 12:47:59 +0100

    Define variable optionals

--- aws-account-setup/variables.tf
+++ aws-account-setup/variables.tf
@@ -2,10 +2,10 @@ variable "management_clusters" {
   type = map(object({
     aws_account = list(object({
       account_id = string
-      aws_partition = string
-      byovpc = bool
-      additional_policies = list(string)
-      additional_policies_arns = list(string)
+      aws_partition = optional(string, "aws")
+      byovpc = optional(bool, false)
+      additional_policies = optional(list(string), [])
+      additional_policies_arns = optional(list(string), [])
     })),
     oidc_provider_domain = string
   }))
