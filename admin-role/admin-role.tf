11e3955afe4f777258d43852ffd9464a1fa725ec
iuri aranda <iuri@giantswarm.io>
2025-01-08 10:14:58 +0100

    Add required providers to admin role module

--- admin-role/admin-role.tf
+++ admin-role/admin-role.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
+
 variable "gs_user_account" {
   type        = string
   description = "Account of Giant Swarm IAM users (`084190472784`, except for China)"
